bae7bedf501041e68af5a8fd5e962fa2d1d73974
Aleksandr <artemof.ru@yandex.ru>
2025-04-10 14:43:50 +0600

    change url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Answer = require("./models/answer");
 const answer = require('./models/answer');
 
 // const URL = "mongodb://127.0.0.1:27017";
-const URL = "mongodb://localhost:27017/quiz";
+const URL = "mongodb://127.0.0.1:27017/quiz";
 
 mongoose
 	.connect(URL)
